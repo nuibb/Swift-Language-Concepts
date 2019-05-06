2b2188c15ebdd738c3e3d4920ba3340b3567bc63
nuibb <nuibb.hasan@gmail.com>
2019-05-06 17:28:35 +0600

    Class, Enumeration, Protocols, Error Handling and Generics learnt in details

--- Swift-Basics.playground/Contents.swift
+++ Swift-Basics.playground/Contents.swift
@@ -158,3 +158,221 @@ print(mappedNumbers)
 //You can refer to parameters by number instead of by name—this approach is especially useful in very short closures. A closure passed as the last argument to a function can appear immediately after the parentheses. When a closure is the only argument to a function, you can omit the parentheses entirely.
 let sortedNumbers = numbers.sorted { $0 < $1 }
 print(sortedNumbers)
+
+//Like classes and all other named types, enumerations can have methods associated with them. By default, Swift assigns the raw values starting at zero and incrementing by one each time, but you can change this behavior by explicitly specifying values. You can also use strings or floating-point numbers as the raw type of an enumeration.
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .jack:
+            return "jack"
+        case .queen:
+            return "queen"
+        case .king:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let jack = Rank.jack
+let jackRawValue = jack.rawValue
+print(jack.simpleDescription())
+
+//Use the init?(rawValue:) initializer to make an instance of an enumeration from a raw value. It returns either the enumeration case matching the raw value or nil if there is no matching Rank.
+if let convertedRank = Rank(rawValue: 12) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
+//The case values of an enumeration are actual values, not just another way of writing their raw values. Here 'Suit' has not any raw type declared, so you can't see any raw values here.
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades"
+        case .hearts:
+            return "hearts"
+        case .diamonds:
+            return "diamonds"
+        case .clubs:
+            return "clubs"
+        }
+    }
+    
+    func colors() -> String {
+        switch self {
+        case .spades, .clubs:
+            return "black"
+        case .diamonds, .hearts:
+            return "red"
+        }
+    }
+}
+
+let hearts = Suit.hearts
+let heartsDescription = hearts.simpleDescription()
+let heartsColor = hearts.colors()
+
+//Another choice for enumeration cases is to have values associated with the case. You can think of the associated values as behaving like stored properties of the enumeration case instance.
+enum ServerResponse {
+    case result(String, String)
+    case failure(String)
+    case noResponse(String)
+}
+
+let success = ServerResponse.result("6:00 am", "8:09 pm")
+let failure = ServerResponse.failure("Out of cheese.")
+let noResponse = ServerResponse.noResponse("Request timeout.")
+
+switch success {
+case let .result(sunrise, sunset):
+    print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
+case let .failure(message):
+    print("Failure...  \(message)")
+case let .noResponse(message):
+    print("Couldn't get any response...  \(message)")
+}
+
+//Use protocol to declare a protocol.
+protocol ExampleProtocol {
+    var simpleDescription: String { get }
+    mutating func adjust() -> String
+}
+
+//Classes, enumerations, and structs can all adopt protocols.
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() -> String {
+        simpleDescription += "  Now 100% adjusted."
+        return simpleDescription
+    }
+}
+var a = SimpleClass()
+print(a.simpleDescription)
+print(a.adjust())
+print(a.simpleDescription)
+
+//Notice the use of the mutating keyword in the declaration of SimpleStructure to mark a method that modifies the structure. The declaration of SimpleClass doesn’t need any of its methods marked as mutating because methods on a class can always modify the class.
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure"
+    mutating func adjust() -> String {
+        simpleDescription += " (adjusted)"
+        return simpleDescription
+    }
+}
+var b = SimpleStructure()
+b.simpleDescription
+b.adjust()
+b.simpleDescription
+
+//You can use an extension to add protocol conformance to a type that is declared elsewhere, or even to a type that you imported from a library or framework.
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() -> String {
+        self += 42
+        return String(self)
+    }
+}
+print(7.simpleDescription)
+//print(7.adjust())//Error : Cannot use mutating member on immutable value: literals are not mutable
+var myNum = 7
+myNum.adjust()
+
+//You can use a protocol name just like any other named type—for example, to create a collection of objects that have different types but that all conform to a single protocol. When you work with values whose type is a protocol type, methods outside the protocol definition are not available.
+let protocolValue: ExampleProtocol = a
+print(protocolValue.simpleDescription)
+//print(protocolValue.anotherProperty)
+//Uncomment to see the error. Even though the variable protocolValue has a runtime type of SimpleClass, the compiler treats it as the given type of ExampleProtocol. This means that you can’t accidentally access methods or properties that the class implements in addition to its protocol conformance.
+
+//Error Handling - You represent errors using any type that adopts the Error protocol.
+enum PrinterError: Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+
+//Use throw to throw an error and throws to mark a function that can throw an error. If you throw an error in a function, the function returns immediately and the code that called the function handles the error.
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+//There are several ways to handle errors. One way is to use do-catch. Inside the do block, you mark code that can throw an error by writing try in front of it. Inside the catch block, the error is automatically given the name error unless you give it a different name.
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Never Has Toner")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+//Another way to handle errors is to use try? to convert the result to an optional. If the function throws an error, the specific error is discarded and the result is nil. Otherwise, the result is an optional containing the value that the function returned.
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+
+//Use defer to write a block of code that is executed after all other code in the function, just before the function returns. The code is executed regardless of whether the function throws an error. You can use defer to write setup and cleanup code next to each other, even though they need to be executed at different times.
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+    
+    let result = fridgeContent.contains(food)
+    return result
+}
+print(fridgeContains("banana"))
+print(fridgeIsOpen)
+
+//Generics - Write a name inside angle brackets to make a generic function or type.
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+makeArray(repeating: fridgeContent, numberOfTimes: 4)
+
+//You can make generic forms of functions and methods, as well as classes, enumerations, and structures.
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+//Use where right before the body to specify a list of requirements—for example, to require the type to implement a protocol, to require two types to be the same, or to require a class to have a particular superclass.
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> [T.Element]
+    where T.Element: Equatable, T.Element == U.Element
+{
+    var result = [T.Element]()
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                result.append(lhsItem)
+            }
+        }
+    }
+    return result
+}
+anyCommonElements([1, 2, 3], [3])
